490a67afe4e79bf3e43f4f3a080cf6be34bee69d
alaxhenry0121 <alaxhenry0121@gmail.com>
2023-01-26 10:42:32 +0900

    Initial Commit

---
+++ AnimationHack/AnimationHackApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationHackApp.swift
+//  AnimationHack
+//
+//  Created by Seungchul Ha on 2023/01/26.
+//
+
+import SwiftUI
+
+@main
+struct AnimationHackApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
